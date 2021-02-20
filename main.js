6602bec14852df6caac3ca42efc587b51ace05d7
Nikki Harper <nicoleharper@ufl.edu>
2021-02-20 10:01:51 -0700

    Make message appear when box #1 is clicked

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 //Hover over any of the boxes change background color of all boxes to red 
 var boxes = document.getElementsByClassName('box');
-var box1 = document.getElementsByClassName('section1'); 
-
 
 function changeToRed() {
     for(i = 0; i < boxes.length; i++) {
     boxes[i].classList.add('red'); 
     }   
-}   
+}; 
 
 function changeBack() {
     for(i = 0; i < boxes.length; i++) {
     boxes[i].classList.remove('red'); 
     }   
-}
+}; 
 
 for(i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener('mouseenter', changeToRed);
     boxes[i].addEventListener('mouseleave', changeBack)
-}
+}; 
+
+
+//Click on box #1 and message appears 
+var box1 = document.getElementsByClassName('section1');
+var newMessage = document.createElement('h2');
+var myNode = document.getElementById('wrapper'); 
+
+box1[0].addEventListener('click', 
+    function() {
+        newMessage.innerHTML = 'Oooh - so close, but no cigar';
+        document.getElementById('wrapper').appendChild(newMessage);
+    }, false
+); 
+
+
+
+// myNode.appendChild(newMessage); 
+
+
+
+
+
